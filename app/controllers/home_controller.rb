2cb804c08c1da9053187ea1786c02ce514d645fc
vajapravin <vajapravin23@gmail.com>
2016-08-27 15:19:33 +0530

    limitted featured movies on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 	def index
-		@featured_movies_1 = Movie.where.not(poster_file_name: nil)
-		@featured_movies_2 = Movie.where.not(poster_file_name: nil)
-		@featured_movies_3 = Movie.where.not(poster_file_name: nil).where.not(youtube: nil)
-		@featured_movies_4 = Movie.where.not(poster_file_name: nil)
+		@featured_movies_1 = Movie.where.not(poster_file_name: nil).limit(10)
+		@featured_movies_2 = Movie.where.not(poster_file_name: nil).where.not(id: @featured_movies_1.pluck(:id)).limit(20)
+		@featured_movies_3 = Movie.where.not(poster_file_name: nil).where.not(id: @featured_movies_1.pluck(:id)).where.not(id: @featured_movies_2.pluck(:id)).where.not(youtube: nil).limit(20)
+		@featured_movies_4 = Movie.where.not(poster_file_name: nil).where.not(id: @featured_movies_1.pluck(:id)).where.not(id: @featured_movies_2.pluck(:id)).where.not(id: @featured_movies_3.pluck(:id)).limit(10)
 	end
 end
