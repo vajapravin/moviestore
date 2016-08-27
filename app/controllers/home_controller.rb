ebca65175465b93c7b7cb4cab6ee1f5636b415e6
vajapravin <vajapravin23@gmail.com>
2016-08-27 14:27:12 +0530

    home page -- done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+	def index
+		@featured_movies_1 = Movie.where.not(poster_file_name: nil)
+		@featured_movies_2 = Movie.where.not(poster_file_name: nil)
+		@featured_movies_3 = Movie.where.not(poster_file_name: nil).where.not(youtube: nil)
+		@featured_movies_4 = Movie.where.not(poster_file_name: nil)
+	end
 end
