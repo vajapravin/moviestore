9d2493fb2f971a7e104eb556ef3bd7e2e8c5d1ae
vajapravin <vajapravin23@gmail.com>
2016-08-27 17:18:55 +0530

    actionpack-page_caching gem implemnted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include ActionController::Caching::Pages if Rails.env.production?
+  if Rails.env.production?
+  	include ActionController::Caching::Pages
+  	self.page_cache_directory = "#{Rails.root.to_s}/public/page_cache"
+  end
 end
